f26d06ae3b565f6ab6fef121995d4cf0918db506
Oleh Pavliuchenko <oleg.s.pavl@gmail.com>
2020-07-05 01:23:16 +0300

    options screen, commented ViewBinding [WIP]

---
+++ sendmoney/src/main/java/com/sendmoney/sendmoney/addmoneyoptions/AddMoneyOptionsFragment.kt
@@ -0,0 +1,24 @@
+package com.sendmoney.sendmoney.addmoneyoptions
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.sendmoney.sendmoney.R
+import com.sendmoney.sendmoney.databinding.FeatureSendmoneyAddmoneyoptionsFragmentBinding
+
+class AddMoneyOptionsFragment : Fragment(R.layout.feature_sendmoney_addmoneyoptions_fragment) {
+    private lateinit var binding: FeatureSendmoneyAddmoneyoptionsFragmentBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FeatureSendmoneyAddmoneyoptionsFragmentBinding.inflate(layoutInflater)
+        view.findViewById<View>(R.id.debitCreditCard).setOnClickListener {}
+        initUI()
+    }
+
+    private fun initUI() {
+//        binding.debitCreditCard.setOnClickListener {
+//            findNavController().navigate()
+//        }
+    }
+}
--- sendmoney/src/main/java/com/sendmoney/sendmoney/main/MainFragment.kt
+++ sendmoney/src/main/java/com/sendmoney/sendmoney/main/MainFragment.kt
@@ -1,26 +1,49 @@
 package com.sendmoney.sendmoney.main
 
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.util.Log
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.sendmoney.sendmoney.R
+import com.sendmoney.sendmoney.databinding.FeatureSendmoneyMainFragmentBinding
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(R.layout.feature_sendmoney_main_fragment) {
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var binding: FeatureSendmoneyMainFragmentBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+    /*override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.feature_sendmoney_main_fragment, container, false)
-    }
+    ): View? {
+        binding = FeatureSendmoneyMainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }*/
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FeatureSendmoneyMainFragmentBinding.inflate(layoutInflater)
+        view.findViewById<Button>(R.id.action).setOnClickListener {
+            Log.e("asdasd", "as!!dasd")
+            findNavController().navigate(MainFragmentDirections.toOptions())
+        }
+        initUI()
+    }
+
+    private fun initUI() {
+//        binding.toolbar.setupWithNavController(findNavController())
+//        binding.action.setOnClickListener {
+//            Log.e("asdasd", "as22dasd")
+//            findNavController().navigate(MainFragmentDirections.toOptions())
+//        }
     }
 }
