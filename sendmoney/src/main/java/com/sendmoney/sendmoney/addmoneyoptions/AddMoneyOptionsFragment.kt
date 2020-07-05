f7901248c45e6acf34fd2c47bca2c58c085198bc
Oleh Pavliuchenko <oleg.s.pavl@gmail.com>
2020-07-05 12:57:21 +0300

    add Card Details screen UI

--- sendmoney/src/main/java/com/sendmoney/sendmoney/addmoneyoptions/AddMoneyOptionsFragment.kt
+++ sendmoney/src/main/java/com/sendmoney/sendmoney/addmoneyoptions/AddMoneyOptionsFragment.kt
@@ -3,6 +3,7 @@ package com.sendmoney.sendmoney.addmoneyoptions
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.sendmoney.sendmoney.R
 import com.sendmoney.sendmoney.databinding.FeatureSendmoneyAddmoneyoptionsFragmentBinding
 
@@ -12,7 +13,9 @@ class AddMoneyOptionsFragment : Fragment(R.layout.feature_sendmoney_addmoneyopti
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FeatureSendmoneyAddmoneyoptionsFragmentBinding.inflate(layoutInflater)
-        view.findViewById<View>(R.id.debitCreditCard).setOnClickListener {}
+        view.findViewById<View>(R.id.debitCreditCard).setOnClickListener {
+            findNavController().navigate(AddMoneyOptionsFragmentDirections.toCarddetails())
+        }
         initUI()
     }
 
---
+++ sendmoney/src/main/java/com/sendmoney/sendmoney/carddetails/CardDetailsFragment.kt
@@ -0,0 +1,6 @@
+package com.sendmoney.sendmoney.carddetails
+
+import androidx.fragment.app.Fragment
+import com.sendmoney.sendmoney.R
+
+class CardDetailsFragment : Fragment(R.layout.feature_sendmoney_carddetails_fragment)
